380db2177b24b1d92690dad49c91802ac24ec708
user <ulanskii03@gmail.com>
2022-02-05 17:18:54 +0600

    Main.java

--- src/peaksoft/Main.java
+++ src/peaksoft/Main.java
@@ -8,12 +8,65 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Class class1 = new Class(34,"красавчик",new int[]{54, 43, 34,45,24,54,13});
-        System.out.println(class1.getWord() +" "+class1.getSan()+" "+ Arrays.toString(class1.getMassiv()));
+        Fish fish = new Fish();
+        fish.setVid("семга");
+        fish.setColour("оранжевый");
+        fish.setSize(25);
+
+        System.out.println("вид: "+fish.getVid());
+        System.out.println("цвет: "+fish.getColour());
+        System.out.println("размер: "+fish.getSize());
+        Fish.swim();
+        Fish.eat();
+        System.out.println("------------------------------");
+
+
+        Parrot parrot = new Parrot();
+        parrot.setName("Пума");
+        parrot.setVid("Корелла");
+        parrot.setColour("серебристый");
+        parrot.setSize(32);
+
+
+        System.out.println("вид: "+parrot.getVid());
+        System.out.println("имя: "+parrot.getName());
+        System.out.println("цвет: "+parrot.getColour());
+        System.out.println("размер: "+parrot.getSize());
+        Parrot.fly();
+        Parrot.repeat();
+
+        System.out.println("________________________________________________");
+
+        Dog dog = new Dog();
+        dog.setBreed(" немецкая овчарка");
+        dog.setName("Рекс");
+        dog.setWeight(40);
+
+        System.out.println("порода: "+dog.getBreed());
+        System.out.println("имя: "+dog.getName());
+        System.out.println("вес: "+dog.getWeight());
+
+        Dog.guard();
+        Dog.run();
+        System.out.println("_________________________________________________________");
+
+
+        Cat cat = new Cat();
+        cat.setBreed("Египетский Сфинкс");
+        cat.setName("Мяу");
+        cat.setYears(7);
+
+        System.out.println("порода: "+cat.getBreed());
+        System.out.println("имя: "+cat.getName());
+        System.out.println("возраст: "+cat.getYears());
+
+        Cat.play();
+        Cat.sleep();
+
+
 
     }
+}
 
-
-    }
 
 
